94c662398b51b4401d4acb788b849e5faacf8018
Ionut Boghean <ionut.boghean.conturi@gmail.com>
2021-01-29 21:23:13 +0000

    JS - testimonials scroll by button

--- js/main.js
+++ js/main.js
@@ -25,13 +25,13 @@ function getDPR () {
 document.addEventListener("DOMContentLoaded", function(){
 
   // collecting data about device
-  let Device = {
+  let device = {
     type: deviceType (),
     dpr: getDPR()
   } 
 
   // menu button for mobile
-  if (Device.type === "mobile") {
+  if (device.type === "mobile") {
 
     // open/close button
     const MenuButton = document.querySelector ("#button-menu-mobile");
@@ -47,101 +47,145 @@ document.addEventListener("DOMContentLoaded", function(){
     }
 
     MenuButton.addEventListener("click", toggleMainMenu );
-  }
-
-  // folder structure (teaching resources page)
-  const resourcesList = document.querySelector("#teaching-resources-list").querySelector("main");
-  const folderList = resourcesList.querySelectorAll(".folder");
-  const subfolderList = resourcesList.querySelectorAll(".subfolder");
-
-  // closing folders and subfolders + adding click events to enable opening them
-
-  // ------------------------------------------------------------------------------
+  }  
   
-  // animation function
-  function startAnimation (element) {
-    // element closing
-    if (element.classList.contains("opened")) {
-      element.style.height = window.getComputedStyle(element).getPropertyValue("height");
+  // testimonials arrows for desktop
+  if (device.type === "computer" && document.querySelector("#testimonials-container")) {
 
-      // return animation duration to be used for delaying (0 would cause cleaning before height has been set)
-      return 10;
-      // return 0;
-    } 
-    // element opening
-    else if (element.classList.contains("closed")) {
-      const children = element.children;
-      let height = parseInt(window.getComputedStyle(element).getPropertyValue("height"));
+    (function(){
+      let currentTestimonial = 1;
+      let scrollPosition = 0;
 
-      // add total height of all children
-      for (let i=1; i<children.length; i++) {
-        height += parseInt(window.getComputedStyle(children[i]).getPropertyValue("height"));
+      const testimonialsParent = document.querySelector("#testimonials");
+      const testimonials = testimonialsParent.children;
+      const nrOfTestimonials = testimonials.length;
+
+      // functions
+      function nextTestimonial () {
+        if (currentTestimonial < nrOfTestimonials) {
+          scrollPosition += parseInt (window.getComputedStyle(testimonials[currentTestimonial-1]).getPropertyValue("width"));
+          testimonialsParent.scrollTo(scrollPosition,0);
+
+          currentTestimonial ++;
+        }
+      }
+  
+      function previousTestimonial () {
+        if (currentTestimonial > 1) {
+          scrollPosition -= parseInt(window.getComputedStyle(testimonials[currentTestimonial-1]).getPropertyValue("width"));
+          testimonialsParent.scrollTo(scrollPosition,0);
+
+          currentTestimonial --;
+        }
+      }
+  
+  
+      // adding event to the buttons
+      const buttonLeft = document.querySelector("#testimonials-container").querySelector(".fa-chevron-left");
+      const buttonRight = document.querySelector("#testimonials-container").querySelector(".fa-chevron-right");
+  
+      buttonLeft.addEventListener("click", previousTestimonial);
+      buttonRight.addEventListener("click", nextTestimonial);
+
+
+    })();
+
+  }
+  
+  // folder structure (teaching resources page)
+  (function () {
+    if(document.querySelector("#teaching-resources-list")) {
+
+      const resourcesList = document.querySelector("#teaching-resources-list").querySelector("main");    
+      const folderList = resourcesList.querySelectorAll(".folder");
+      const subfolderList = resourcesList.querySelectorAll(".subfolder");
+
+      // closing folders and subfolders + adding click events to enable opening them
+      
+      // animation function
+      function startAnimation (element) {
+        // element closing
+        if (element.classList.contains("opened")) {
+          element.style.height = window.getComputedStyle(element).getPropertyValue("height");
+
+          // return animation duration to be used for delaying (0 would cause cleaning before height has been set)
+          return 10;
+          // return 0;
+        } 
+        // element opening
+        else if (element.classList.contains("closed")) {
+          const children = element.children;
+          let height = parseInt(window.getComputedStyle(element).getPropertyValue("height"));
+
+          // add total height of all children
+          for (let i=1; i<children.length; i++) {
+            height += parseInt(window.getComputedStyle(children[i]).getPropertyValue("height"));
+          }
+
+          // set height to element -> the change will start animation
+          element.style.height = height + "px";
+
+          // return animation duration to be used for delaying 
+          let delay = 1000 * parseFloat(window.getComputedStyle(element).getPropertyValue("transition-duration"));
+          return delay;
+        }
       }
 
-      // set height to element -> the change will start animation
-      element.style.height = height + "px";
-
-      // return animation duration to be used for delaying 
-      let delay = 1000 * parseFloat(window.getComputedStyle(element).getPropertyValue("transition-duration"));
-      return delay;
-    }
-  }
-
-  // cleaning function
-  function cleanAfterAnimation (element) {
-      element.removeAttribute("style");
-    }
-
-  // ------------------------------------------------------------------------------
-  
-  // subFolders
-  for (let i=0; i<subfolderList.length; i++) {
-    subfolderList[i].classList.remove("opened");
-    subfolderList[i].classList.add("closed");
-    subfolderList[i].style.cursor = "pointer";
+      // cleaning function
+      function cleanAfterAnimation (element) {
+          element.removeAttribute("style");
+        }
     
-    subfolderList[i].addEventListener ("click", function (e) {
+      // subFolders
+      for (let i=0; i<subfolderList.length; i++) {
+        subfolderList[i].classList.remove("opened");
+        subfolderList[i].classList.add("closed");
+        subfolderList[i].style.cursor = "pointer";
+        
+        subfolderList[i].addEventListener ("click", function (e) {
 
-      e.stopPropagation(); // prevent toggeling parent or child folders/subfolders
+          e.stopPropagation(); // prevent toggeling parent or child folders/subfolders
 
-      let delay = startAnimation (subfolderList[i]); // due to height having to be auto
-      
-      setTimeout( function () {
-        subfolderList[i].classList.toggle("closed");
-        subfolderList[i].classList.toggle("opened");
-      }, delay );
+          let delay = startAnimation (subfolderList[i]); // due to height having to be auto
+          
+          setTimeout( function () {
+            subfolderList[i].classList.toggle("closed");
+            subfolderList[i].classList.toggle("opened");
+          }, delay );
 
-      setTimeout( function () {
-        cleanAfterAnimation (subfolderList[i]); // due to height having to be auto
-      }, delay );
- 
-    });
+          setTimeout( function () {
+            cleanAfterAnimation (subfolderList[i]); // due to height having to be auto
+          }, delay );
     
-  }
-  
-  // folders
-  for (let i = 0; i<folderList.length; i++) {
-    folderList[i].classList.remove("opened");
-    folderList[i].classList.add("closed");
-    folderList[i].style.cursor = "pointer";
+        });
+        
+      }
+    
+      // folders
+      for (let i = 0; i<folderList.length; i++) {
+        folderList[i].classList.remove("opened");
+        folderList[i].classList.add("closed");
+        folderList[i].style.cursor = "pointer";
 
-    folderList[i].addEventListener ("click", function (e) {
+        folderList[i].addEventListener ("click", function (e) {
 
-      e.stopPropagation();
+          e.stopPropagation();
 
-      let delay = startAnimation (folderList[i]); // due to height having to be auto
+          let delay = startAnimation (folderList[i]); // due to height having to be auto
 
-      setTimeout( function () {
-        folderList[i].classList.toggle("closed");
-        folderList[i].classList.toggle("opened");
-      }, delay );
+          setTimeout( function () {
+            folderList[i].classList.toggle("closed");
+            folderList[i].classList.toggle("opened");
+          }, delay );
 
-      setTimeout( function () {
-        cleanAfterAnimation (folderList[i]); // due to height having to be auto
-      }, delay );
+          setTimeout( function () {
+            cleanAfterAnimation (folderList[i]); // due to height having to be auto
+          }, delay );
 
-    });
-  }
+        });
+      }
+    }
+  })();
 
 });
 
